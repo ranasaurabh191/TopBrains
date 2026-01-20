865e9eeb9bc088776f60a71a54c0a7b1fd8f966c
~SRB <ranasaurabh191@gmail.com>
2026-01-20 15:27:10 +0530

    q16

---
+++ GCD/Program.cs
@@ -0,0 +1,20 @@
+﻿
+public class Program
+{
+    public static int GCD(int a, int b)
+    {
+        if (b == 0)
+            return a;
+
+        return GCD(b, a % b); // recursion
+    }
+
+    public static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int result = GCD(a, b);
+        Console.WriteLine(result);
+    }
+}
