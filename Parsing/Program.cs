de887e4595009ee4c9285cbf72a4fc6add147930
~SRB <ranasaurabh191@gmail.com>
2026-01-20 15:22:42 +0530

    q15

---
+++ Parsing/Program.cs
@@ -0,0 +1,31 @@
+﻿
+public class Program
+{
+    public static int SumParsedIntegers(string[] tokens)
+    {
+        int sum = 0;
+        
+        foreach (string token in tokens)
+        {
+            if (int.TryParse(token, out int value)) // check krne k liye, chks the range while parsing 
+            {
+                sum += value;
+            }
+        }
+        return sum;
+    }
+
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] tokens = new string[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            tokens[i] = Console.ReadLine();
+        }
+
+        int result = SumParsedIntegers(tokens);
+        Console.WriteLine(result);
+    }
+}
