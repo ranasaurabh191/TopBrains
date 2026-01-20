d64de020fd80769f7e65abb669d66c6ebb7d7773
~SRB <ranasaurabh191@gmail.com>
2026-01-20 14:57:43 +0530

    q10

---
+++ Display Height/Program.cs
@@ -0,0 +1,17 @@
+﻿
+public class Program
+{
+    public static string GetHeightCategory(int height)
+    {
+        if (height < 150) return "Short";
+        else if (height < 180) return "Average";
+        else return "Tall";
+    }
+
+    public static void Main()
+    {
+        int heightCm = Convert.ToInt32(Console.ReadLine());
+        string category = GetHeightCategory(heightCm);
+        Console.WriteLine(category);
+    }
+}
