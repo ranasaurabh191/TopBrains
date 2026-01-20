2e0b08625d3d886c179d19ebc4f682822631db77
~SRB <ranasaurabh191@gmail.com>
2026-01-20 15:04:16 +0530

    q12

---
+++ Time Conversion/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class Program
+{
+    public static string ConvertToMinutesSeconds(int totalSeconds)
+    {
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        return minutes + ":" + seconds.ToString("D2"); //"D2" ensures leading zero like 02
+    }
+
+    public static void Main()
+    {
+        int totalSeconds = Convert.ToInt32(Console.ReadLine());
+        string result = ConvertToMinutesSeconds(totalSeconds);
+        Console.WriteLine(result);
+    }
+}
+
+git add .
+git commit -m "q12"
+git push
