8fc05020c17ba7b27e91e4533ae3d042fd691316
~SRB <ranasaurabh191@gmail.com>
2026-01-20 15:11:26 +0530

    q14

---
+++ Multiplication Table/Program.cs
@@ -0,0 +1,28 @@
+﻿
+public class Program
+{
+    public static int[] GetMultiplicationRow(int n, int upto)
+    {
+        int[] row = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            row[i - 1] = n * i; // bcz indexing is from 0
+        }
+
+        return row;
+    }
+
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int upto = Convert.ToInt32(Console.ReadLine());
+
+        int[] result = GetMultiplicationRow(n, upto);
+
+        foreach (int value in result)
+        {
+            Console.Write(value + " ");
+        }
+    }
+}
