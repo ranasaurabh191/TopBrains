2d76974ccc97399037579a8bce04e07b3386f690
~SRB <ranasaurabh191@gmail.com>
2026-01-20 14:55:13 +0530

    q9

---
+++ Conversion/Program.cs
@@ -0,0 +1,16 @@
+﻿
+public class Program
+{
+    public static double ConvertFeetToCentimeters(int feet)
+    {
+        double centimeters = feet * 30.48;
+        return Math.Round(centimeters, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main()
+    {
+        int feet = Convert.ToInt32(Console.ReadLine());
+        double result = ConvertFeetToCentimeters(feet);
+        Console.WriteLine(result);
+    }
+}
