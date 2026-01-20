4dcd03a5edf4a9a18bc21b78d1751beee1b1e44d
~SRB <ranasaurabh191@gmail.com>
2026-01-20 16:04:42 +0530

    q18

---
+++ Object Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static int SumOnlyIntegers(object[] values)
+    {
+        int sum = 0;
+
+        foreach (object item in values)
+        {
+            if (item is int x) // checking
+            {
+                sum += x;
+            }
+        }
+        return sum;
+    }
+
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        object[] values = new object[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            values[i] = Console.ReadLine(); // mixed types simulated as strings
+        }
+        int result = SumOnlyIntegers(values);
+        Console.WriteLine(result);
+    }
+}
