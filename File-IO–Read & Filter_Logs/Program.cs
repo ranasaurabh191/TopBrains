4e2796d16cbbc30661481d10fda34337e3936a1d
~SRB <ranasaurabh191@gmail.com>
2026-01-20 10:54:43 +0530

    q5

---
+++ File-IO–Read & Filter_Logs/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        string inputFile = "log.txt";    
+        string outputFile = "error.txt";  
+
+    
+        string[] lines = File.ReadAllLines(inputFile);
+
+        // error.txt file create / overwrite karo
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            // Har line ko check karo
+            foreach (string line in lines)
+            {
+                // Agar line me "ERROR" hai
+                if (line.Contains("ERROR"))
+                {
+                    writer.WriteLine(line); // To error.txt me likh do
+                }
+            }
+        }
+
+        Console.WriteLine("ERROR logs extracted successfully.");
+    }
+}
