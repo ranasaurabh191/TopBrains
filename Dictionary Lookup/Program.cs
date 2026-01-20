7ad1dbe0ac1fdc45aaed5eb1d146c5e7516bd4ad
~SRB <ranasaurabh191@gmail.com>
2026-01-20 14:44:18 +0530

    q6

---
+++ Dictionary Lookup/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        int[] ids = { 1, 4, 5 };
+
+        Dictionary<int, int> salaries = new Dictionary<int, int>
+        {
+            { 1, 20000 },
+            { 4, 40000 },
+            { 5, 15000 }
+        };
+
+        int totalSalary = 0;
+
+        for (int i = 0; i < ids.Length; i++)
+        {
+            totalSalary += salaries[ids[i]];
+        }
+
+        Console.WriteLine(totalSalary);
+    }
+}
