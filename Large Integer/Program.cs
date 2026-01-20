467cadba7104569a9ffc8a1946f08b016590b882
~SRB <ranasaurabh191@gmail.com>
2026-01-20 15:00:08 +0530

    q11

---
+++ Large Integer/Program.cs
@@ -0,0 +1,20 @@
+﻿
+public class Program
+{
+    public static int GetLargest(int a, int b, int c)
+    {
+        if (a >= b && a >= c) return a;
+        else if (b >= a && b >= c) return b;
+        else return c;
+    }
+
+    public static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int largest = GetLargest(a, b, c);
+        Console.WriteLine(largest);
+    }
+}
