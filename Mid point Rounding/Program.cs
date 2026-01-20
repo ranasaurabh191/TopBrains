a7c47b7fa9a0f5d412df2237b833423c8db0bf1a
~SRB <ranasaurabh191@gmail.com>
2026-01-20 14:53:18 +0530

    q8

---
+++ Mid point Rounding/Program.cs
@@ -0,0 +1,16 @@
+﻿
+public class Program
+{
+    public static double CalculateCircleArea(double radius)
+    {
+        double area = Math.PI * radius * radius;
+        return Math.Round(area, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main()
+    {
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double result = CalculateCircleArea(radius);
+        Console.WriteLine(result);
+    }
+}
