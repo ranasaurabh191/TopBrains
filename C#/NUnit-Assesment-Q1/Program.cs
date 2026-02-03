894d45ae12ddbdc9a34127c765e50e0e1ed1fa6d
~SRB <ranasaurabh191@gmail.com>
2026-02-03 16:14:02 +0530

    SQL11

---
+++ C#/NUnit-Assesment-Q1/Program.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+class UnitTests
+{
+    public void Test_Deposit_ValidAmount()
+    {
+        
+    }
+    public void Test_Deposit_NegativeAmount()
+    {
+        
+    }
+    public void Test_Withdraw_ValidAmount()
+    {
+        
+    }
+    public void Test_Withdraw_InsufficientFunds()
+    {
+        
+    }
+
+}
