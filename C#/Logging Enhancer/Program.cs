d36dfe87c6023bdfc954fd2770be0e87847eb2ac
Saurabh Rana <ranasaurabh191@gmail.com>
2026-02-09 19:16:53 +0530

    Logging Enhancer

---
+++ C#/Logging Enhancer/Program.cs
@@ -0,0 +1,28 @@
+﻿
+static class LogExtensions
+{
+    public static void ToInfoLog(this string message)
+    {
+        Console.WriteLine($"[INFO] [{DateTime.Now}] {message}");
+    }
+
+    public static void ToWarningLog(this string message)
+    {
+        Console.WriteLine($"[WARNING] [{DateTime.Now}] {message}");
+    }
+
+    public static void ToErrorLog(this string message)
+    {
+        Console.WriteLine($"[ERROR] [{DateTime.Now}] {message}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        "User logged in".ToInfoLog();
+        "Low disk space".ToWarningLog();
+        "Payment failed".ToErrorLog();
+    }
+}
