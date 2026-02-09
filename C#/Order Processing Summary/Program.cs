4f4d8bb95961704ad015602b24231c4e6db3bd3e
Saurabh Rana <ranasaurabh191@gmail.com>
2026-02-09 19:50:11 +0530

    Order processing Summary

---
+++ C#/Order Processing Summary/Program.cs
@@ -0,0 +1,21 @@
+﻿
+class OrderSystem
+{
+    static void Main()
+    {
+        var result = ProcessOrder(101);
+
+        Console.WriteLine($"Total: {result.totalAmount}");
+        Console.WriteLine($"Premium: {result.isPremiumCustomer}");
+        Console.WriteLine($"Delivery: {result.deliveryStatus}");
+    }
+
+    static (decimal totalAmount, bool isPremiumCustomer, string deliveryStatus) ProcessOrder(int orderId)
+    {
+        decimal totalAmount = 12500;
+        bool isPremium = totalAmount > 10000;
+        string delivery = isPremium ? "Express" : "Standard";
+
+        return (totalAmount, isPremium, delivery);
+    }
+}
