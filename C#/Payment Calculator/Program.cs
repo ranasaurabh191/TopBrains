83b1a1ddc94c319d9bded8971eaf06d001815143
Saurabh Rana <ranasaurabh191@gmail.com>
2026-02-09 19:19:06 +0530

    Payment Calculator

---
+++ C#/Payment Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class PaymentCalculator
+{
+    static void Main()
+    {
+        decimal baseAmount = 6000;
+        decimal tax, discount, finalAmount;
+
+        CalculateBill(ref baseAmount, out tax, out discount, out finalAmount);
+
+        Console.WriteLine($"Base Amount (After Discount): {baseAmount}");
+        Console.WriteLine($"Tax: {tax}");
+        Console.WriteLine($"Final Amount: {finalAmount}");
+    }
+
+    static void CalculateBill(ref decimal baseAmount, out decimal tax, out decimal discount, out decimal finalAmount)
+    {
+        discount = baseAmount > 5000 ? baseAmount * 0.10m : 0;
+        baseAmount -= discount;
+
+        tax = baseAmount * 0.18m;
+        finalAmount = baseAmount + tax;
+    }
+}
