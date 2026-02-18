1e12a017c2429a2564bcd4ec447d911072121c5f
Saurabh Rana <ranasaurabh191@gmail.com>
2026-02-18 11:45:37 +0530

    SmartBankingSystem

--- C#/Smart Banking System/Program.cs
+++ C#/Smart Banking System/Program.cs
@@ -13,19 +13,14 @@ class Program
         };
 
         Console.WriteLine("Balance > 50000");
-        accounts.Where(a => a.Balance > 50000)
-                .ToList().ForEach(a => Console.WriteLine(a.CustomerName));
+        accounts.Where(a => a.Balance > 50000).ToList().ForEach(a => Console.WriteLine(a.CustomerName));
 
         Console.WriteLine("Total Bank Balance: " + accounts.Sum(a => a.Balance));
 
         Console.WriteLine("Top 3 Accounts:");
-        accounts.OrderByDescending(a => a.Balance)
-                .Take(3).ToList()
-                .ForEach(a => Console.WriteLine(a.CustomerName));
+        accounts.OrderByDescending(a => a.Balance).Take(3).ToList().ForEach(a => Console.WriteLine(a.CustomerName));
 
         Console.WriteLine("Grouped by Type:");
-        accounts.GroupBy(a => a.GetType().Name)
-                .ToList().ForEach(g =>
-                Console.WriteLine(g.Key + " : " + g.Count()));
+        accounts.GroupBy(a => a.GetType().Name).ToList().ForEach(g =>  Console.WriteLine(g.Key + " : " + g.Count()));
     }
 }
