f4002c87c57c69e1e3c4e6eceff9e8e9f4b9ed13
Saurabh Rana <ranasaurabh191@gmail.com>
2026-02-21 09:49:20 +0530

    VS

--- M1 Questions/College Management/Program.cs
+++ M1 Questions/College Management/Program.cs
@@ -41,26 +41,150 @@ public class Program
 
         public int AddStudent(string studentId, string subject, int marks)
         {
+            if (!studentRecords.ContainsKey(studentId))
+            {
+                studentRecords[studentId] = new Dictionary<string, int>();
+                studentSubjectsOrder[studentId] = new LinkedList<KeyValuePair<string, int>>();
+            }
+            if (!studentRecords[studentId].ContainsKey(subject))
+            {
+                studentRecords[studentId][subject] = marks;
+                studentSubjectsOrder[studentId].AddLast(
+                    new KeyValuePair<string, int>(subject, marks));
+            }
+            else if (marks > studentRecords[studentId][subject])
+            {
+                studentRecords[studentId][subject] = marks;
 
+                var node = studentSubjectsOrder[studentId].First;
+                while (node != null)
+                {
+                    if (node.Value.Key == subject)
+                    {
+                        node.Value = new KeyValuePair<string, int>(subject, marks);
+                        break;
+                    }
+                    node = node.Next;
+                }
+            }
+
+            if (!subjectsRecords.ContainsKey(subject))
+            {
+                subjectsRecords[subject] = new Dictionary<string, int>();
+                subjectsStudentsOrder[subject] = new LinkedList<KeyValuePair<string, int>>();
+            }
+
+            if (!subjectsRecords[subject].ContainsKey(studentId))
+            {
+                subjectsRecords[subject][studentId] = marks;
+                subjectsStudentsOrder[subject].AddLast(
+                    new KeyValuePair<string, int>(studentId, marks));
+            }
+            else if (marks > subjectsRecords[subject][studentId])
+            {
+                subjectsRecords[subject][studentId] = marks;
+
+                var node = subjectsStudentsOrder[subject].First;
+                while (node != null)
+                {
+                    if (node.Value.Key == studentId)
+                    {
+                        node.Value = new KeyValuePair<string, int>(studentId, marks);
+                        break;
+                    }
+                    node = node.Next;
+                }
+            }
+
+            return 1;
         }
 
         public int RemoveStudent(string studentId)
         {
+            if (!studentRecords.ContainsKey(studentId))
+                return 0;
 
+            foreach (var subject in studentRecords[studentId].Keys)
+            {
+                subjectsRecords[subject].Remove(studentId);
+
+                var node = subjectsStudentsOrder[subject].First;
+                while (node != null)
+                {
+                    if (node.Value.Key == studentId)
+                    {
+                        subjectsStudentsOrder[subject].Remove(node);
+                        break;
+                    }
+                    node = node.Next;
+                }
+            }
+
+            studentRecords.Remove(studentId);
+            studentSubjectsOrder.Remove(studentId);
+
+            return 1;
         }
 
         public string TopStudent(string subject)
         {
-            
+            if (!subjectsRecords.ContainsKey(subject))
+                return "";
+
+            int maxMarks = subjectsRecords[subject].Values.Max();
+            List<string> res = new();
+
+            foreach (var kv in subjectsStudentsOrder[subject])
+            {
+                if (kv.Value == maxMarks)
+                    res.Add($"{kv.Key} {kv.Value}");
+            }
+
+            return string.Join("\n", res);
         }
 
         public string Result()
         {
-            
+            List<string> res = new();
+
+            foreach (var student in studentSubjectsOrder)
+            {
+                double avg = student.Value.Average(x => x.Value);
+                res.Add($"{student.Key} {avg:F2}");
+            }
+
+            return string.Join("\n", res);
         }
     }
+
     public static void Main()
     {
+        CollageManagement cm = new();
+        string input;
 
+        while (!string.IsNullOrEmpty(input = Console.ReadLine()))
+        {
+            string[] parts = input.Split(' ');
+            string command = parts[0];
+
+            switch (command)
+            {
+                case "ADD":
+                    cm.AddStudent(parts[1], parts[2], int.Parse(parts[3]));
+                    break;
+
+                case "REMOVE":
+                    cm.RemoveStudent(parts[1]);
+                    break;
+
+                case "TOP":
+                    Console.WriteLine(cm.TopStudent(parts[1]));
+                    break;
+
+                case "RESULT":
+                    Console.WriteLine(cm.Result());
+                    break;
+            }
+        }
     }
 }
