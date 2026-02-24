6e4f20eb4df2a72cd869b2800160044fe7b31875
Saurabh Rana <ranasaurabh191@gmail.com>
2026-02-24 14:48:18 +0530

    Input from console

---
+++ JavaScript/InputFromConsole.js
@@ -0,0 +1,10 @@
+var readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Please enter your name: ', (name)=> {
+    console.log(`Hello, ${name}!`);
+    readline.close();
+});
+
